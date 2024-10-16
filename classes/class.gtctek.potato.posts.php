dc557dddd8054b0ea694df375698a91e12411678
Nick Watt <nick@gtctek.co.uk>
2024-10-16 23:02:08 +0100

    Tidied up posts class

--- classes/class.gtctek.potato.posts.php
+++ classes/class.gtctek.potato.posts.php
@@ -1,11 +1,23 @@
 <?php
-
-class GTCTEK_Potato_Posts {
-    private static $initiated = false;
+    class GTCTEK_Potato_Posts {
+        private static $initiated = false;
     
-    public static function init() {
-        if ( ! self::$initiated ) {
-            self::init_hooks();
+        public static function init() {
+            if ( ! self::$initiated ) {
+                self::init_hooks();
+            }
+        }
+
+        private static function init_hooks() {
+            add_shortcode( 'gtc_posts_list', array( 'GTCTEK_Potato_Posts', 'posts_list' ) );
+            self::$initiated = true;
+        }
+
+        public static function posts_list( $atts ) {
+            return "boobs";
+            ?>
+            <p>More boobies</p>
+            <?php
         }
     }
-}
+?>
