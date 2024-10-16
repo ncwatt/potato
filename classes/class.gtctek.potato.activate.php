8a26c82cb751c27e9ee1b8f051d14cc7973d6faa
Nick Watt <nick@gtctek.co.uk>
2024-10-16 23:01:49 +0100

    Tidied up activate class

--- classes/class.gtctek.potato.activate.php
+++ classes/class.gtctek.potato.activate.php
@@ -1,15 +1,15 @@
 <?php
-
-class GTCTEK_Potato_Activate {
-    private static $initiated = false;
+    class GTCTEK_Potato_Activate {
+        private static $initiated = false;
     
-    public static function init() {
-        if ( ! self::$initiated ) {
-            self::init_hooks();
+        public static function init() {
+            if ( ! self::$initiated ) {
+                self::init_hooks();
+            }
+        }
+
+        private static function init_hooks() {
+            self::$initiated = true;
         }
     }
-
-    private static function init_hooks() {
-        self::$initiated = true;
-    }
-}
+?>
